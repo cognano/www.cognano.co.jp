fb1271bb3b65d68abbef4be232334c443e68d429
linyows <linyows@gmail.com>
2022-06-12 21:39:34 +0900

    add translations for japanese

---
+++ i18n/ja.tsx
@@ -0,0 +1,24 @@
+export default {
+  hello: 'やあ！',
+  header: {
+    language: `言語`,
+    news: `お知らせ`,
+    projects: `プロジェクト`,
+    blog: `ブログ`,
+    about: `私たちについて`,
+    contact: `お問合わせ`,
+  },
+  index: {
+    heroMessage: `COGNANOは、コンピューターを利用した創薬を目指すベンチャー企業です。
+      アルパカから得られた大きなVHHデータは、私たちを新しい創薬プラットフォームに導きます。
+      私たちのゴールは、薬のデザイン/開発を最適化することです。`,
+    aboutUs: `私たちについて`,
+    latestPosts: `最近のブログ`,
+    viewAllPosts: `ブログ一覧へ`,
+    latestProjects: `最近のプロジェクト`,
+    viewAllProjects: `プロジェクト一覧へ`,
+  },
+  privacy: {
+    modified: `このプライバシーポリシーの最終更新日は%sです。`,
+  }
+}
