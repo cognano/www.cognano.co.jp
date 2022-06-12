f9e021917713013d1e1a602488675b4f4110b14a
linyows <linyows@gmail.com>
2022-06-12 21:39:19 +0900

    add translations for english

---
+++ i18n/en.tsx
@@ -0,0 +1,24 @@
+export default {
+  hello: 'Yo',
+  header: {
+    language: `Language`,
+    news: `News`,
+    projects: `Projects`,
+    blog: `Blog`,
+    about: `About`,
+    contact: `Contact`,
+  },
+  index: {
+    heroMessage: `COGNANO is a venture to aim computer-supported drug discovery.
+      The big VHH data obtained from our own alpacas will lead us to a new drug discovery platform.
+      Our goal is to optimize drug designing/development.`,
+    aboutUs: `About Us`,
+    latestPosts: `Latest posts`,
+    viewAllPosts: `View all posts`,
+    latestProjects: `Latest Projects`,
+    viewAllProjects: `View all projects`,
+  },
+  privacy: {
+    modified: `This privacy policy was last modified on %s.`,
+  }
+}
