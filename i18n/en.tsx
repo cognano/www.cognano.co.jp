ca2de7de27d0d7ecb3a4de3e953e142a2fa7a5b2
linyows <linyows@gmail.com>
2022-06-15 00:06:54 +0900

    add translations for cookie consent

--- i18n/en.tsx
+++ i18n/en.tsx
@@ -8,6 +8,10 @@ export default {
     about: `About`,
     contact: `Contact`,
   },
+  cookie: {
+    message: `We serve cookies to analyse traffic to this site. Information about your use of this site is shared with we for that purpose.`,
+    buttonText: `OK, got it`,
+  },
   index: {
     heroMessage: `COGNANO is a venture to aim computer-supported drug discovery.
       The big VHH data obtained from our own alpacas will lead us to a new drug discovery platform.
