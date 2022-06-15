f418dc021d84b2bf3e551ca0418661ff70af141b
linyows <linyows@gmail.com>
2022-06-15 23:11:13 +0900

    add ga component

---
+++ components/ga.tsx
@@ -0,0 +1,70 @@
+/**
+ * Document:
+ * https://nextjs.org/docs/messages/next-script-for-ga
+ * Example:
+ * https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+ */
+import React, { ReactNode, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+
+type Props = {
+  children?: ReactNode
+}
+
+const GAID = process.env.NEXT_PUBLIC_GA_ID
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  window.gtag('config', GAID, {
+    page_path: url,
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}
+
+const GA: React.FC<Props> = ({ children }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    console.log(GAID)
+    const handleRouteChange = (url) => {
+      pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GAID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GAID}');
+        `}
+      </Script>
+    </>
+  )
+}
+
+export default GA
