a378f6694d6f8ae182d0b9bffe9aa4744616572c
linyows <linyows@gmail.com>
2023-02-06 20:55:40 +0900

    add language component

---
+++ components/language.tsx
@@ -0,0 +1,55 @@
+import React, { ReactNode, useState } from 'react'
+import { languageIcon, sortdownIcon } from './icons'
+import { useRouter } from 'next/router'
+import styles from '../styles/Language.module.css'
+import t, { lang } from '../i18n'
+
+type Props = {
+  children?: ReactNode
+  top?: boolean
+}
+
+const Language: React.FC<Props> = ({ children, top }) => {
+  const [open, setOpen] = useState(false)
+  const onClick = () => setOpen(!open)
+  const router = useRouter()
+
+  const locationHref = (l: string) => {
+    const uri = decodeURI(router.asPath)
+    const domain = l === 'en' ? 'cognanous.com' : 'www.cognano.co.jp'
+    const url = `https://${domain}${uri}?lang=${l}`
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`location href: ${url}`)
+    } else if (document.location.host !== domain) {
+      document.location.href = url
+    }
+  }
+  const toEnglish = () => locationHref('en')
+  const toJapanese = () => locationHref('ja')
+
+  return (
+    <div className={styles.languages} onClick={onClick}>
+      <span className={styles.languageIcon}>{languageIcon()}</span>
+      <span>{t(`language.${lang}`)}</span>
+      <span className={styles.sortdownIcon}>{sortdownIcon()}</span>
+      {open && <div className={`${styles.switcher} ${top ? styles.switcherTop : null}`}>
+        <ul>
+          <li onClick={toEnglish}>
+            <div>
+              {t('language.en')}
+              <span className={styles.langen}>English (US)</span>
+            </div>
+          </li>
+          <li onClick={toJapanese}>
+            <div>
+              {t('language.ja')}
+              <span className={styles.langen}>日本語</span>
+            </div>
+          </li>
+        </ul>
+      </div>}
+    </div>
+  )
+}
+
+export default Language
