0c826beaf88fa508effc9c0463ea3be2e00e8cef
linyows <linyows@gmail.com>
2023-05-08 23:33:02 +0900

    simplize

--- lib/il6.ts
+++ lib/il6.ts
@@ -4,30 +4,28 @@ import {
   QueryDatabaseResponseEx,
 } from 'notionate'
 
-const motherlibrariesQuery = {
-    database_id: process.env.NOTION_MOTHERLIBRARIES_DB_ID,
-    sorts: [
-      {
-        property: 'Name',
-        direction: 'ascending'
-      },
-    ]
-} as QueryDatabaseParameters
-
 export const GetMomLibs = async (): Promise<QueryDatabaseResponseEx> => {
-  return await FetchDatabase(motherlibrariesQuery)
+  const q = {
+      database_id: process.env.NOTION_MOTHERLIBRARIES_DB_ID,
+      sorts: [
+        {
+          property: 'Name',
+          direction: 'ascending'
+        },
+      ]
+  } as QueryDatabaseParameters
+  return await FetchDatabase(q)
 }
 
-const sublibrariesQuery = {
-    database_id: process.env.NOTION_SUBLIBRARIES_DB_ID,
-    sorts: [
-      {
-        property: 'Name',
-        direction: 'ascending'
-      },
-    ]
-} as QueryDatabaseParameters
-
 export const GetSubLibs = async (): Promise<QueryDatabaseResponseEx> => {
-  return await FetchDatabase(sublibrariesQuery)
+  const q = {
+      database_id: process.env.NOTION_SUBLIBRARIES_DB_ID,
+      sorts: [
+        {
+          property: 'Name',
+          direction: 'ascending'
+        },
+      ]
+  } as QueryDatabaseParameters
+  return await FetchDatabase(q)
 }
