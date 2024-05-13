037d082c3e013ffea7f2e969577e89bb739801f0
linyows <linyows@gmail.com>
2024-05-13 15:16:36 +0900

    support empty variables for unsplash component

--- components/unsplash.tsx
+++ components/unsplash.tsx
@@ -3,15 +3,23 @@ import styles from '../styles/App.module.css'
 
 type Props = {
   children?: ReactNode
-  href: string
-  name: string
+  href?: string
+  name?: string
 }
 
 const Unsplash: React.FC<Props> = ({ children, href, name }) => {
   const unsplashUrl = `https://unsplash.com`
+  if (href && name) {
+    return (
+      <p className={styles.unsplash}>
+        Photo by <a href={href} target="_blank" rel="noopener noreferrer">{name}</a> on <a href={unsplashUrl} target="_blank" rel="noopener noreferrer">Unsplash</a>
+      </p>
+    )
+  }
+
   return (
     <p className={styles.unsplash}>
-      Photo by <a href={href} target="_blank" rel="noopener noreferrer">{name}</a> on <a href={unsplashUrl} target="_blank" rel="noopener noreferrer">Unsplash</a>
+      Photo by <a href={unsplashUrl} target="_blank" rel="noopener noreferrer">Unsplash</a>
     </p>
   )
 }
